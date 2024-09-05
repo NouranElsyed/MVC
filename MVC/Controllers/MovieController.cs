a2876189def60ef8f179f59ab0378c88f68474f7
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-09-05 17:12:45 +0300

    completed

---
+++ MVC/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        public string Welcome (string Name) 
+        {
+            return $"Hello {Name}\nWelcome to our website";
+        }
+        [HttpGet]
+        [ActionName("Movie")]
+        public IActionResult GetMovie(int id , string name , Movie movie) 
+        {
+            return Content($"Id: {id}");
+        }
+
+        //public string Index()
+        //{
+        //    return $"Hello Welcome to our website";
+        //}
+        public RedirectToActionResult Test(int id) 
+        {
+            RedirectToActionResult result = new RedirectToActionResult("Index","Home",new { id ="2"});
+            return result; 
+        }
+        public IActionResult GetMovie(int id)
+        {
+            
+            return Content($"Helo this your movie {id}",  "text/html");
+        }
+
+
+    }
+}
